a0735cfd83b9b570e7b8f38dec1d4b9431d330df
Tobias Refsholt <tobias@refsholt.net>
2023-10-20 13:52:13 +0200

    Options-page

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,11 @@
 "use strict";
+
+window.addEventListener("load", () => {
+    updateView();
+})
+
+function initGame() {
+    model.app.boardSize = model.fields.options.boardSize;
+    model.app.page = "activeGame";
+    updateView();
+}
--- js/model.js
+++ js/model.js
@@ -1,5 +1,13 @@
 "use strict";
 
 const model = {
-    
+    app: {
+        page: "options",
+        boardSize: null,
+    },
+    fields: {
+        options: {
+            boardSize: 3,
+        }
+    }
 }
