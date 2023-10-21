7d71506915121a3acc935266b3fe27b3345f5401
Tobias Refsholt <tobias@refsholt.net>
2023-10-21 14:23:50 +0200

    CSS styling and x-svg

--- js/view.js
+++ js/view.js
@@ -15,22 +15,24 @@ function getOptionsHTML() {
     return /* html */`
         <h2>Velg størrelse</h2>
         <div class="choose-board-size">
-            <label for="board-size-3x3">
-                <input type="radio" name="board-size" id="board-size-3x3" onfocus="model.fields.options.boardSize = 3" checked="true">
-                3x3
-            </label>
-            <label for="board-size-5x5">
-                <input type="radio" name="board-size" id="board-size-5x5" onfocus="model.fields.options.boardSize = 5">
-                5x5
-            </label>
-            <label for="board-size-10x10">
-                <input type="radio" name="board-size" id="board-size-10x10" onfocus="model.fields.options.boardSize = 10">
-                10x10
-            </label>
-            <label for="board-size-15x15">
-                <input type="radio" name="board-size" id="board-size-15x15" onfocus="model.fields.options.boardSize = 15">    
-                15x15
-            </label>
+            <div class="radio-buttons">
+                <label for="board-size-3x3">
+                    <input type="radio" name="board-size" id="board-size-3x3" onfocus="model.fields.options.boardSize = 3" checked="true">
+                    3x3
+                </label>
+                <label for="board-size-5x5">
+                    <input type="radio" name="board-size" id="board-size-5x5" onfocus="model.fields.options.boardSize = 5">
+                    5x5
+                </label>
+                <label for="board-size-10x10">
+                    <input type="radio" name="board-size" id="board-size-10x10" onfocus="model.fields.options.boardSize = 10">
+                    10x10
+                </label>
+                <label for="board-size-15x15">
+                    <input type="radio" name="board-size" id="board-size-15x15" onfocus="model.fields.options.boardSize = 15">    
+                    15x15
+                </label>
+            </div>
             <button onclick="initGame()">Start spill</button>
         </div>
     `;
